abee45b5da442712134295cf44c1285da53f2756
Christoph Nicolai <mail@christoph-nicolai.de>
2024-09-11 18:11:53 +0200

    Refactor get_canvas function to accept highlights and notes

--- obsidian.py
+++ obsidian.py
@@ -1,33 +1,27 @@
 import uuid
 
-EXAMPLES = [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-]
-
-
-PATH = "/Users/christoph/Documents/Obsidian/Zettelkasten/Canvas"
-
 
 def get_uuid():
     while True:
         yield uuid.uuid4().hex[:16]
 
 
-def get_canvas(EXAMPLES, width=800, height=400, padding=20):
+def get_canvas(hightlights_and_notes, width=800, height=400, padding=20):
     nodes = [
         {
             "id": next(get_uuid()),
             "type": "text",
-            "text": example,
+            "text": (
+                highlight[0]
+                if highlight[1] == ""
+                else highlight[0] + "\n> " + highlight[1]
+            ),
             "x": 0,
             "y": 0 + idx * (height + padding),
             "width": width,
             "height": height,
         }
-        for idx, example in enumerate(EXAMPLES)
+        for idx, highlight in enumerate(hightlights_and_notes)
     ]
     canvas = {"nodes": nodes, "edges": []}
     return canvas
